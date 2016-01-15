8c65fcbab033eb8f54e79eac9351a671e4a9a1ed
Jomit <jomitv@live.com>
2016-01-15 12:39:20 -0800

    added sidebar toggle and basic transitions

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿
+(function () {
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    alert("hello");
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if($sidebarAndWrapper.hasClass("hide-sidebar")){
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
