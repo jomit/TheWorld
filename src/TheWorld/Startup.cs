8bf39db5d01a3a4939130e91fe64d339973a3c52
Jomit <jomitv@live.com>
2016-01-15 11:37:27 -0800

    Updated layout and added css for the sidebar and main content wrapper

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -20,10 +20,12 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync($"Hello World! {context.Request.Path}");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync($"Hello World! {context.Request.Path}");
+            //});
+            app.UseDefaultFiles();  //Use the default index.html or default.html file
+            app.UseStaticFiles();
         }
 
         // Entry point for the application.
