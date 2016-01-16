bb406e4a46561f0e14bf6df74d7aa5833efc1bb5
Jomit <jomitv@live.com>
2016-01-15 17:48:54 -0800

    added service dependency for sending email. finished contact form with validation and model state.

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNet.Mvc;
 using System;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
-    public class AppController: Controller
+    public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +26,32 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)  //Server side validation of the model
+            {
+                var fromEmail = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(fromEmail))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem");
+                }
+
+                if (_mailService.SendMail(
+                    fromEmail,
+                    fromEmail,
+                    $"Contact Page from {model.Name} ({model.Email})",
+                    model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent. Thanks !";
+                }
+            }
             return View();
         }
     }
