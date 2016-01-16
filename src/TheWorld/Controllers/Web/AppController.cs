37e2be2cc3f26179ab8371ae9d4f34b87c05872d
Jomit <jomitv@live.com>
2016-01-15 16:40:44 -0800

    Enabled MVC6

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
