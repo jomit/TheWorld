7e41266f901983997fa2245678a802aaf60d3092
Jomit <jomitv@live.com>
2016-01-15 17:21:23 -0800

    added simple view model and validation for view model via tag helpers

---
+++ src/TheWorld/ViewModel/ContractViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(255,MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1034, MinimumLength =5)]
+        public string Message { get; set; }
+    }
+}
