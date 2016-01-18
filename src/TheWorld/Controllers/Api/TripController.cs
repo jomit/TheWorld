36cf77647b1cfcd26bcd40a8b4f99daa029c9374
Jomit <jomitv@live.com>
2016-01-17 16:41:44 -0800

    Integrated AutoMapper and viewmodel for trip model validation

--- src/TheWorld/Controllers/Api/TripController.cs
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -4,25 +4,62 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using TheWorld.Models;
+using System.Net;
+using TheWorld.ViewModel;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TheWorld.Controllers.Api
 {
+    [Route("api/trips")]
     public class TripController : Controller
     {
+        private ILogger<TripController> logger;
         private IWorldRepository repository;
 
-        public TripController(IWorldRepository repo)
+        public TripController(IWorldRepository repo, ILogger<TripController> log)
         {
             repository = repo;
+            logger = log;
         }
 
-        [HttpGet("api/trips")]
+        [HttpGet("")]
         public JsonResult Get()
         {
-            var results = repository.GetAllTripsWtihStops();
+            var results = Mapper.Map<IEnumerable<TripViewModel>>(repository.GetAllTripsWtihStops());
             return Json(results);
+        }
+
+        [HttpPost("")]
+        public JsonResult Post([FromBody] TripViewModel vm)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newTrip = Mapper.Map<Trip>(vm);
+
+                    //Save to the database
+                    logger.LogInformation("Starting to Save a new Trip");
+
+
+
+
+                    Response.StatusCode = (int)HttpStatusCode.Created;
+                    return Json(Mapper.Map<TripViewModel>(newTrip));
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Failed to Save New Trip", ex);
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json(new { Message = ex.Message });
+            }
+
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return Json(new { Message = "Failed", ModelState = ModelState });
         }
     }
 }
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.PlatformAbstractions;
 using TheWorld.Models;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
+using AutoMapper;
+using TheWorld.ViewModel;
 
 namespace TheWorld
 {
@@ -59,6 +61,12 @@ namespace TheWorld
             loggerFactory.AddDebug(LogLevel.Warning);
 
             app.UseStaticFiles();
+
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<Trip, TripViewModel>().ReverseMap();
+                //config.CreateMap<TripViewModel, Trip>();
+            });
 
             app.UseMvc(config =>
             {
