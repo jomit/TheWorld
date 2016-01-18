81b564c9937808a52a0e3d1dfe05ce1abb262fc7
Jomit <jomitv@live.com>
2016-01-17 16:09:17 -0800

    added new trips api and configured the camel case contract resolver. tested the api using "PostMan" tool

---
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using TheWorld.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        private IWorldRepository repository;
+
+        public TripController(IWorldRepository repo)
+        {
+            repository = repo;
+        }
+
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+            var results = repository.GetAllTripsWtihStops();
+            return Json(results);
+        }
+    }
+}
